24b25cd7f67830532b2661988812e61299f025bc
BaekSeungHo <vudrkd1580@naver.com>
2023-03-10 18:56:36 +0900

    Complete Converter

--- src/App.js
+++ src/App.js
@@ -2,12 +2,57 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
-import Translate from "./component/Translate";
+import MinutesToHours from "./component/MinutesToHours";
+import KmToMiles from "./component/KmToMiles";
+import Rsp from "./component/Rsp";
+import KgToLb from "./component/KgToLb";
+import CelsiusToFahrenheit from "./component/CelsiusToFahrenheit";
+import Speed from "./component/Speed";
+import Energy from "./component/Energy";
+import React from "react";
 
 function App() {
+  const [index, setIndex] = useState(-1);
+  const onSelect = (event) => {
+    setIndex(event.target.value);
+  };
+  const [isListHover, setIsListHover] = useState(false);
   return (
     <div className="App">
-      <Translate />
+      <div className="wrapper">
+        <h1 style={{ color: "skyblue" }}>My Converter</h1>
+        <select value={index} onChange={onSelect}>
+          <option value="-1"> Select your units</option>
+          <option value="0">Minutes & Hours</option>
+          <option value="1">Km & miles</option>
+          <option value="2">Kg & Lb</option>
+          <option value="3">Celsius & Fahrenheit</option>
+          <option value="4">MeterSecond & HourKilometer</option>
+          <option value="5">Joule & kiloCalorie</option>
+          <option value="6">Rock Paper Scissors</option>
+        </select>
+        <div style={{ fontSize: "25px", fontWeight: "700" }}>
+          {index == "-1" ? (
+            <div>
+              Please select your unit{" "}
+              <img
+                onMouseOver={() => setIsListHover(true)}
+                onMouseOut={() => setIsListHover(false)}
+                src={isListHover ? "pepe2.avif" : "pepe1.png"}
+                style={{ width: "100%" }}
+              />
+            </div>
+          ) : null}
+          {index === "0" ? <MinutesToHours /> : null}
+          {index === "1" ? <KmToMiles /> : null}
+          {index === "2" ? <KgToLb /> : null}
+          {index === "3" ? <CelsiusToFahrenheit /> : null}
+          {index === "4" ? <Speed /> : null}
+          {index === "5" ? <Energy /> : null}
+          {index === "6" ? <Rsp /> : null}
+          <div></div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/component/CelsiusToFahrenheit.js
@@ -0,0 +1,91 @@
+import { useState } from "react";
+import React from "react";
+
+const styles = {
+  button: {
+    height: "50px",
+    width: "48%",
+    margin: "0 3px",
+    cursor: "pointer",
+  },
+  mainDiv: {
+    height: "350px",
+  },
+  img: {
+    width: "100px",
+    height: "110px",
+  },
+  div: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    textAlign: "center",
+  },
+  input: {
+    width: "200px",
+    height: "40px",
+    fontSize: "20px",
+  },
+  label: {
+    fontWeight: "700",
+  },
+};
+
+function CelsiusToFahrenheit() {
+  const [amount, setAmount] = React.useState(0.0);
+  const [inverted, setInverted] = React.useState(false);
+  const onChange = (event) => {
+    setAmount(event.target.value);
+  };
+
+  const reset = () => setAmount(0);
+  const onInverted = () => {
+    reset();
+    setInverted((current) => !current);
+  };
+  return (
+    <div>
+      <div style={styles.mainDiv}>
+        <div style={styles.div}>
+          <label style={styles.label} htmlFor="minutes">
+            Celsius
+          </label>
+          <input
+            style={styles.input}
+            value={inverted ? (((amount - 32) * 5) / 9).toFixed(2) : amount}
+            id="minutes"
+            placehoder="Minutes"
+            type="number"
+            onChange={onChange}
+            disabled={inverted}
+          />
+        </div>
+        <div style={styles.div}>
+          <img style={styles.img} src="pngwing.com.png" alt="trans" />
+        </div>
+        <div style={styles.div}>
+          <label style={styles.label} htmlFor="hours">
+            Fahrenheit
+          </label>
+          <input
+            style={styles.input}
+            value={inverted ? amount : ((amount * 9) / 5 + 32).toFixed(2)}
+            id="hours"
+            placehoder="Hours"
+            type="number"
+            disabled={!inverted}
+            onChange={onChange}
+          />
+        </div>
+      </div>
+      <button style={styles.button} onClick={reset}>
+        Reset
+      </button>
+      <button style={styles.button} onClick={onInverted}>
+        {inverted ? "Turn back" : "Inverted"}
+      </button>
+    </div>
+  );
+}
+
+export default CelsiusToFahrenheit;
